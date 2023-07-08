d9f30bce441f1e963fe4fb9abec7a78dd866c8db
LDirk <47995676+LDirk@users.noreply.github.com>
2023-07-08 18:35:47 -0300

    Update flows.py

--- flows.py
+++ flows.py
@@ -1,19 +1,17 @@
-from prefect import Flow, Parameter
-
-from tasks import SCRIPT_ETL
-
-with Flow('Ocorrencias') as flow:
-    SCRIPT_ETL()
-
-
-
 from prefect import Flow
+from datetime import datetime, timedelta
 from tasks import SCRIPT_ETL
+from prefect.schedules import IntervalSchedule
+
 
 schedule = IntervalSchedule(
     start_date=datetime.utcnow() + timedelta(seconds=1),
+    end_date = datetime.utcnow() + timedelta(seconds=3601),
     interval=timedelta(minutes=1),
 )
+
+with Flow('Ocorrencias', schedule = schedule) as flow:
+    SCRIPT_ETL()
 
 with Flow('Ocorrencias', schedule=schedule) as flow_f:
     SCRIPT_ETL()
