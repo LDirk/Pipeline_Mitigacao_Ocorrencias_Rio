eb5da43f3a96034fa957bd0e841ec3e4fed4b03d
LDirk <47995676+LDirk@users.noreply.github.com>
2023-07-08 23:01:51 -0300

    Update test.py

--- dados/test.py
+++ dados/test.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df_verificar = pd.read_csv('dados_ocorrencias2.csv', encoding='latin1')
+df_verificar = pd.read_csv('dados_ocorrencias2.csv', encoding='utf-8')
 
 pd.set_option('display.max_columns', None)
 
