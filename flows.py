33f5eaea45445ed3abed1c7c7680ec9079ba12ba
LDirk <47995676+LDirk@users.noreply.github.com>
2023-07-08 21:17:09 -0300

    Update flows.py

--- flows.py
+++ flows.py
@@ -6,8 +6,8 @@ from prefect.schedules import IntervalSchedule
 
 schedule = IntervalSchedule(
     start_date=datetime.utcnow() + timedelta(seconds=1),
-    end_date = datetime.utcnow() + timedelta(seconds=3601),
-    interval=timedelta(minutes=1),
+    end_date = datetime.utcnow() + timedelta(seconds=3650),
+    interval=timedelta(minutes=20),
 )
 
 with Flow('Ocorrencias', schedule = schedule) as flow:
